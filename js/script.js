ba3d0f5c526d3b827179c3d4d4b5bb27dc2825c4
Brandon Brandhuber <brandon.brandhuber@stu.ocsb.ca>
2025-05-01 15:52:59 +0000

    HTML Movie

--- js/script.js
+++ js/script.js
@@ -1,5 +1,30 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 BrandonBCode All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: BrandonBCode
+// Created on: March 2025
 // This file contains the JS functions for index.html
+
+"use strict";
+/**
+ * This function calculates the celsius from fahrenheit
+ */
+function intakeAge() {
+    // input that takes the fahrenheit from the user and converts it.
+    let userAge = parseInt(document.getElementById("userAge").value);
+    let display = "";
+
+    if (userAge >= 17){
+        display = "You can see a Rated R Movie Alone!"
+    }
+    else if (userAge >= 13) {
+        display = "You can see a PG-13 movie alone!"
+    } else if (userAge >= 5) {
+        display = "You can see a G or PG movie alone!"
+    } else {
+        display = "You're too young for most movies unfortunately"
+    }
+
+  // output that displays the celsius
+    document.getElementById("outputAge").innerHTML = display;
+
+}
